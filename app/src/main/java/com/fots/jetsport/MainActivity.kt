1a9cc1b41bb416f292a57382fdb4bd53039c0410
fevziomurtekin <fevziomurtekin@gmail.com>
2021-04-03 22:57:30 +0300

    initial commit

---
+++ app/src/main/java/com/fots/jetsport/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.fots.jetsport
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fots.jetsport.ui.theme.JetsportTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetsportTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("JetSport")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(modifier = Modifier
+            .fillMaxSize(0.5f)
+            .fillMaxHeight(),
+            text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetsportTheme {
+        Greeting("Android")
+    }
+}
